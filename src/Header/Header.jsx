3515ee266c143c9e924fa178916227ab39b6dfcb
Mazen Ahmed <mazen.00ahmd@gmail.com>
2025-09-08 17:30:53 +0300

    Initial Commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,32 @@
+import styles from "./Header.module.css"
+import LogoPic from "../assets/flower.png"
+
+function Header(){
+
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.logoContainer}>
+                <img alt="Bloom Watch Logo" src={LogoPic} className={styles.logoPic}></img>
+                <h1 className={styles.logoName}>Bloom Watch</h1>
+            </div>
+            <nav>
+                <ul className={styles.navList}>
+                    <li className={styles.navItem}><a href={"#"}>Home</a></li>
+                    <li className={styles.navItem}><a href={"#"}>Dashboard</a></li>
+                    <li className={styles.navItem}>
+                        <button>Predictions <span className={styles.arrow}>&#x25BC;</span></button>
+                        <div className={styles.dropDownMenuContainer}>
+                            <ul className={styles.dropDownMenu}>
+                                <li>Model 1</li>
+                                <li>Model 2</li>
+                                <li>Model 3</li>
+                            </ul>
+                        </div>
+                        </li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+}
+export default Header
